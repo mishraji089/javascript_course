36911ce60fe2fea1c42bedd3d3f4a22392197aca
dhruv <mishradhruv089@gmail.com>
2025-09-18 10:05:42 +0530

    javascript index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// const arr=[1,2,3,4,2,3,5,6,7,5];
+// const unique=[...new Set(arr)];             to remove duplicates
+// console.log(unique);
+
+// Q2
+// if(5=='5') {                // checks only values         (5==='5')  checks both values and types and return boolean true of false
+//     console.log("yes");
+// }
+// else{
+//  console.log("no");
+// }    
+
+
+
+
+
